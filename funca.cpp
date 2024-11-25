5fd0c9ab16d51b23d9d4c581042e713e6fd8384e
vitaliydenisko <vip.vitalii.2004@ukr.net>
2024-11-25 18:40:40 +0200

    Finalized implementation and added comments

--- funca.cpp
+++ funca.cpp
@@ -1,10 +1,12 @@
 #include "funca.h"
 #include <cmath>
 
+// calculate - функція для обчислення суми перших n елементів ряду
+// n - кількість елементів для обчислення
 double FuncA::calculate(int n) {
     double result = 0.0;
     for (int i = 0; i < n; ++i) {
-        result += pow(-1, i) * (pow(2 * i, 2)) / (pow(2 * i + 1, 2)); // Формула
+        result += pow(-1, i) * (pow(2 * i, 2)) / (pow(2 * i + 1, 2));
     }
     return result;
 }
