c7251238bfebac1c6ca2774c58cd2d8c8cd3bf9b
vitaliydenisko <vip.vitalii.2004@ukr.net>
2024-11-25 18:34:07 +0200

    Implemented calculate function with infinite series

--- funca.cpp
+++ funca.cpp
@@ -1,6 +1,11 @@
 #include "funca.h"
+#include <cmath>
 
 double FuncA::calculate(int n) {
-    return 0; // Повертає 0 як початковий результат
+    double result = 0.0;
+    for (int i = 0; i < n; ++i) {
+        result += pow(-1, i) * (pow(2 * i, 2)) / (pow(2 * i + 1, 2)); // Формула
+    }
+    return result;
 }
 
