04484222c966b4ad7499538bfd146e35ed073518
vitaliydenisko <vip.vitalii.2004@ukr.net>
2024-11-25 18:36:49 +0200

    Fixed calculate function to return sum of first 3 terms

--- funca.cpp
+++ funca.cpp
@@ -1,6 +1,11 @@
 #include "funca.h"
+#include <cmath>
 
 double FuncA::calculate(int n) {
-    return 0; // Повертає 0 як початковий результат
+    double result = 0.0;
+    for (int i = 0; i < 3; ++i) { // Лише перші 3 елементи
+        result += pow(-1, i) * (pow(2 * i, 2)) / (pow(2 * i + 1, 2));
+    }
+    return result;
 }
 
