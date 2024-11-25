d1bbcb11f1221bf66ec90c26fadcb639bb9091b9
vitaliydenisko <vip.vitalii.2004@ukr.net>
2024-11-25 19:42:18 +0200

    Resolved merge conflict in funca.cpp on/ branchMake

--- funca.cpp
+++ funca.cpp
@@ -5,10 +5,10 @@
 // n - кількість елементів для обчислення
 double FuncA::calculate(int n) {
     double result = 0.0;
-    for (int i = 0; i < 3; ++i) { // Лише перші 3 елементи
     for (int i = 0; i < n; ++i) {
         result += pow(-1, i) * (pow(2 * i, 2)) / (pow(2 * i + 1, 2));
     }
     return result;
 }
+
 
